1606a7cc72f070435140814a5f7b9248918841c8
Dia <p.porntipa1997@gmail.com>
2025-05-29 15:51:05 +1000

    Added Australia time to pcs data

--- Utils/datetime.py
+++ Utils/datetime.py
@@ -8,3 +8,13 @@ def add_UTC_Datetime(df):
     df['UTC_Datetime'] = df['Datetime'].dt.tz_convert('UTC')
 
     return df
+
+
+def add_Australia_Datetime(df):
+    aus_tz = timezone('Australia/Sydney')
+    df = df[~df['UTC_Datetime'].isna()]
+    if df['UTC_Datetime'].dt.tz is None:
+        df['UTC_Datetime'] = df['UTC_Datetime'].dt.tz_localize('UTC')
+    df['Datetime'] = df['UTC_Datetime'].dt.tz_convert(aus_tz)
+
+    return df
