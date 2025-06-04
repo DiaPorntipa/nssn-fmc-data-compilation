65c325e486aaf85d6ab73f1c845697e4274c76e8
Dia <p.porntipa1997@gmail.com>
2025-06-04 12:09:42 +1000

    Aligned Datetime and UTC_Datetime formats

--- Utils/datetime.py
+++ Utils/datetime.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from pytz import timezone
 
 
@@ -7,14 +8,20 @@ def add_UTC_Datetime(df):
     df = df[~df['Datetime'].isna()]
     df['UTC_Datetime'] = df['Datetime'].dt.tz_convert('UTC')
 
+    df['Datetime'] = df['Datetime'].apply(lambda t: pd.to_datetime(t).tz_localize(None))
+    df['UTC_Datetime'] = df['UTC_Datetime'].apply(lambda t: pd.to_datetime(t).tz_localize(None))
+
     return df
 
 
-def add_Australia_Datetime(df):
+def add_Canberra_Datetime(df):
     aus_tz = timezone('Australia/Sydney')
     df = df[~df['UTC_Datetime'].isna()]
     if df['UTC_Datetime'].dt.tz is None:
         df['UTC_Datetime'] = df['UTC_Datetime'].dt.tz_localize('UTC')
     df['Datetime'] = df['UTC_Datetime'].dt.tz_convert(aus_tz)
+
+    df['Datetime'] = df['Datetime'].apply(lambda t: pd.to_datetime(t).tz_localize(None))
+    df['UTC_Datetime'] = df['UTC_Datetime'].apply(lambda t: pd.to_datetime(t).tz_localize(None))
 
     return df
